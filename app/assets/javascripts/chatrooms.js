1b8030189217c8004d95ef93d4cae34256a7dc1e
dinhthai2206 <dinhthai2206@gmail.com>
2019-04-22 17:08:05 +0700

    add web socket

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -2,7 +2,16 @@ $(document).on("turbolinks:load", function() {
   $("#new_message").on("keypress", function(e) {
     if (e && e.keyCode === 13) {
       e.preventDefault();
-      Rails.fire($(this)[0], 'submit');
+      $(this).submit();
     }
+  });
+
+  $("#new_message").on("submit", function(e) {
+    e.preventDefault();
+    chatroom_id = $("[data-behavior='messages']").data("chatroom-id");
+    content = $("#message_content");
+
+    App.chatrooms.send_message(chatroom_id, content.val());
+    content.val("")
   });
 });
